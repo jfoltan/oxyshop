de80f75ba65b8471b5586328f9de113fb6a7b1d4
Jakub Foltan <jakub.foltan@outlook.cz>
2023-09-24 12:07:40 +0200

    feat: show error messeages when bad data send to create_user API endpoint and throw 400 response

--- src/Controller/UserApiController.php
+++ src/Controller/UserApiController.php
@@ -9,16 +9,31 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[Route('/api', name: 'api_')]
 class UserApiController extends AbstractController
 {
     #[Route('/users', name: 'create_user')]
-    public function createUser(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    public function createUser(Request $request, EntityManagerInterface $entityManager, ValidatorInterface $validator): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
 
         $user = new User();
+
+        $errors = $validator->validate($user);
+
+        if (count($errors) > 0)
+        {
+            $errorMesseages = [];
+            foreach ($errors as $error)
+            {
+                $errorMesseages[] = $error->getMessage();
+            }
+
+            return new JsonResponse(['errors' => $errorMesseages], Response::HTTP_BAD_REQUEST);
+        }
+
         $user->setName($data['name']);
         $user->setPassword($data['password']);
         $user->setEmail($data['email']);
